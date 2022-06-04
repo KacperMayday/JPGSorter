cfc9f9760a2e82b0c9ccfc2e1f337871cf9f116d
Jakub Chrupek <jakub.chrupek@hotmail.com>
2022-06-04 11:32:42 +0200

    added file scanning and metadata colllection

--- src/main/java/pl/wit/projekt/JPGScanner.java
+++ src/main/java/pl/wit/projekt/JPGScanner.java
@@ -1,19 +1,29 @@
 package pl.wit.projekt;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 
 public class JPGScanner {
+	
 	// Data utworzenia -> Lista nazw plików
-	private Map<Date, List<String>> mapMetaFilenames;
+	private Map<FileTime, ArrayList<Path>> filesMetadata;
 
-	public JPGScanner(String sourcePath) {
+	public JPGScanner(Path path) throws IOException {
 		// inicjalizacja mapy
-		mapMetaFilenames = null;
+		filesMetadata = null;
 
-		List<String> listFilenames = scanDirectory(sourcePath);
-		getMetadata(listFilenames);
+		List<Path> sourceFiles = scanDirectory(path);
+		getMetadata(sourceFiles);
 	}
 
 	/**
@@ -21,8 +31,20 @@ public class JPGScanner {
 	 * 
 	 * @param sourcePath
 	 */
-	private List<String> scanDirectory(String sourcePath) {
-		return null;
+	private List<Path> scanDirectory(Path path) throws IOException {
+			
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Path must be a directory!");
+        }
+
+        List<Path> result;
+        try (Stream<Path> walk = Files.walk(path)) {
+            result = walk
+                    .filter(Files::isRegularFile) // is a file
+                    .filter(p -> p.getFileName().toString().endsWith(".jpg"))
+                    .collect(Collectors.toList());
+        }
+        return result;
 	}
 
 	/**
@@ -30,11 +52,32 @@ public class JPGScanner {
 	 * 
 	 * @param listFilenames
 	 */
-	private void getMetadata(List<String> listFilenames) {
-
-	}
-
-	public Map<Date, List<String>> getMapMetaFilenames() {
-		return mapMetaFilenames;
+	private void getMetadata(List<Path> paths) {
+		paths.forEach(path -> {
+			BasicFileAttributes attributes = null;
+	        try
+	        {
+	            attributes = Files.readAttributes(path, BasicFileAttributes.class); // zczytanie atrybutow pliku 
+	        }
+	        catch (IOException exception)
+	        {
+	            System.out.println("Exception handled when trying to get file " +
+	                    "attributes: " + exception.getMessage());
+	        }
+	        
+	        FileTime createdAt = attributes.creationTime();
+	        
+	        ArrayList<Path> inMap = this.filesMetadata.get(createdAt);
+	        
+	        if(inMap == null) {
+	        	inMap = new ArrayList<Path>();
+	        	inMap.add(path);
+	            this.filesMetadata.put(createdAt, inMap);
+	       } else {
+	           // add if item is not already in list
+	           if(!inMap.contains(path)) inMap.add(path);
+	       }
+		});
+	
 	}
 }
