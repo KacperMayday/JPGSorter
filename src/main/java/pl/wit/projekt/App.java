7a093ca8817f953c159b3bc15049d06a1c7aba30
KacperMayday <madej.kcpr@gmail.com>
2022-06-05 14:36:03 +0200

    Dodanie flow do klasy głównej App + dodanie katalogu na testowe obrazki

--- src/main/java/pl/wit/projekt/App.java
+++ src/main/java/pl/wit/projekt/App.java
@@ -1,18 +1,27 @@
 package pl.wit.projekt;
 
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.util.List;
+import java.util.Map;
+
 public class App {
 	/**
 	 * parsowanie argumentów (wraz z ich walidacją) i uruchomienie głównej klasy
 	 * JPGUtility
-	 * 
-	 * @param args
+	 *
 	 */
 	public static void main(String[] args) {
 		// robocza inicjalizacja parametrów
 		int threadPool = 0;
-		String sourcePath = null;
-		String targetPath = null;
+		String sourcePath = "";
+		String targetPath = "";
 
-		JPGUtility project = new JPGUtility(threadPool, sourcePath, targetPath);
+		// flow
+		JPGScanner scanner = new JPGScanner(sourcePath);
+		Map<FileTime, List<Path>> mapa = scanner.getMetadata();
+
+		JPGUtility utility = new JPGUtility(threadPool, Path.of(targetPath));
+		utility.copyFiles(mapa);
 	}
 }
