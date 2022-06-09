3f4918cd05b2c7b66d9258dcb2d11399114aa84d
KacperMayday <madej.kcpr@gmail.com>
2022-06-09 23:33:12 +0200

    Rozbudowa testu jednostkowego JPGUtilityTest

--- src/test/java/pl/wit/projekt/JPGUtilityTest.java
+++ src/test/java/pl/wit/projekt/JPGUtilityTest.java
@@ -73,7 +73,9 @@ public class JPGUtilityTest {
 		cleanDirectory(new File(targetPath.toString()));
 
 		// mapa plików do skopiowania
-		mapMetaFilenames = Map.of(LocalDate.parse("2022-06-05"), List.of(sourcePath.resolve("test_image1.jpg")));
+		mapMetaFilenames = Map.of(
+				LocalDate.parse("2022-06-08"), List.of(sourcePath.resolve("test_image1.jpg"), sourcePath.resolve("test_image2.jpg")),
+				LocalDate.parse("2022-06-09"), List.of(sourcePath.resolve("test_image2.jpg")));
 	}
 
 	/**
